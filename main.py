b6f2fb06862b6ddf68e1919f5f485350a7f83ca2
akilpath <56412097+akilpath@users.noreply.github.com>
2023-04-15 22:02:52 -0400

    Created model one

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# import tensorflow as tf
+#
+# converter = tf.lite.TFLiteConverter.from_saved_model("./")
+# tfLiteModel = converter.convert()
+# with open('model20230415.tflite', 'wb') as file:
+#     file.write(tfLiteModel)
+# print('done')
+
 import pathlib
 import tensorflow as tf
 
@@ -6,15 +14,15 @@ IMAGE_SIZE = (480, 640)
 # batch size
 BATCH_SIZE = 16
 
-dataseturl = "C:/Users/Admin/Desktop/dataset"
-training_ds = tf.keras.utils.image_dataset_from_directory(dataseturl,
+datasetDir = "./dataset"
+training_ds = tf.keras.utils.image_dataset_from_directory(datasetDir,
                                                           validation_split=0.2,
                                                           subset="training",
                                                           seed=123,
                                                           image_size=IMAGE_SIZE,
                                                           batch_size=BATCH_SIZE)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(dataseturl,
+val_ds = tf.keras.utils.image_dataset_from_directory(datasetDir,
                                                      validation_split=0.2,
                                                      subset="validation",
                                                      seed=123,
@@ -38,18 +46,22 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, 3, padding='same', activation="relu"),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(128, activation="relu"),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(numOfClasses)
+    tf.keras.layers.Dense(1, activation="sigmoid")
 ])
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss="binary_crossentropy",
               metrics=["accuracy"])
 #model.summary()
-epochs = 9
+epochs = 20
 history = model.fit(
     training_ds,
     validation_data = val_ds,
     epochs = epochs
 )
+
+if input() == "y":
+    tf.saved_model.save(model, "./")
