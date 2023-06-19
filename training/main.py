bb48d2b7598359400c763298a534c649f2b47113
akilpath <56412097+akilpath@users.noreply.github.com>
2023-06-18 22:05:39 -0400

    Added training data for model 3

--- training/main.py
+++ training/main.py
@@ -8,6 +8,7 @@
 
 import pathlib
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 # image height x width
 IMAGE_SIZE = (500, 500)
@@ -35,7 +36,8 @@ numOfClasses = len(classNames)
 training_ds = training_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Rescaling(1. / 255, input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)),
+    tf.keras.layers.RandomBrightness(0.3, input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)),
+    tf.keras.layers.Rescaling(1. / 255),
     tf.keras.layers.Conv2D(16, 3, padding='same', activation="relu"),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Conv2D(32, 3, padding='same', activation="relu"),
@@ -60,12 +62,35 @@ history = model.fit(
     validation_data=val_ds,
     epochs=epochs
 )
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+fig = plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='upper left')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+fig.savefig("training_accuracy.png", dpi=200)
 
 print("Save Model?")
 if input() == "y":
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tfLiteModel = converter.convert()
     print("Converted to tflite obj")
-    with open('model20230618.tflite', 'wb') as file:
+    with open('model3.tflite', 'wb') as file:
         file.write(tfLiteModel)
     print('done')
